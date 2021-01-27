0720eb30ea581f75cf620a46c8d299c5ef9e3f5b
Mariana Antunes <marianajantunes97@gmail.com>
2021-01-26 22:57:01 -0300

    Criação das Views (pages): Agenda, Clientes, Procedimentos e Funcionários

--- View/MenuView.xaml.cs
+++ View/MenuView.xaml.cs
@@ -1,28 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SalaoDeCabelereiro.View
 {
     /// <summary>
     /// Interação lógica para MenuView.xam
     /// </summary>
-    public partial class MenuView : Page
+    public partial class MenuView : Window
     {
         public MenuView()
         {
             InitializeComponent();
+        }
+
+        private void ControlaGridCursor(RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursor.Margin = new Thickness(10 + (150 * index), 30, 0, 0);
+        }
+
+        private void BtnMenu(object sender, RoutedEventArgs e)
+        {
+            ControlaGridCursor(e);
+            FrameMain.Content = null;
+        }
+
+        private void BtnAgenda(object sender, RoutedEventArgs e)
+        {
+            ControlaGridCursor(e);
+            FrameMain.Content = new AgendaView();
+        }
+
+        private void BtnClientes(object sender, RoutedEventArgs e)
+        {
+            ControlaGridCursor(e);
+            FrameMain.Content = new ClienteView();
+        }
+
+        private void BtnFuncionarios(object sender, RoutedEventArgs e)
+        {
+            ControlaGridCursor(e);
+            FrameMain.Content = new FuncionarioView();
+        }
+
+        private void BtnProcedimentos(object sender, RoutedEventArgs e)
+        {
+            ControlaGridCursor(e);
+            FrameMain.Content = new ProcedimentoView();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
